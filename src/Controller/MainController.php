d4dca123c8df68b6be51c82315fa37cd44283e6f
Salhi-Yassine <boy.yassine@hotmail.com>
2024-04-09 23:37:15 +0000

    Creating your own service

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/')]
-    public function homePage(): Response
+    public function homePage(StarshipRepository $starshipRepository): Response
     {
-        $starshipCount = 457;
-
-        $myShip = [
-            'name' => 'USS LeafyCruiser (NCC-0001)',
-            'class' => 'Garden',
-            'captain' => 'Jean-Luc Pickles',
-            'status' => 'under construction',
-        ];
+        $ships = $starshipRepository->findAll();
+        $starshipCount = count($ships);
+        $myShip = $ships[array_rand($ships)];
 
         return $this->render('main/homepage.html.twig', [
             'numberOfStarships' => $starshipCount,
