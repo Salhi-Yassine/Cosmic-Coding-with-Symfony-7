ba38e0aec1b03539a8b05e163e9d6d273bfee355
Salhi-Yassine <boy.yassine@hotmail.com>
2024-04-09 03:05:01 +0000

    Creating JSON API Endpoints

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
@@ -20,7 +20,7 @@ class MainController extends AbstractController
             'status' => 'under construction',
         ];
 
-        return $this->render('main/homepage.html.twig',[
+        return $this->render('main/homepage.html.twig', [
             'numberOfStarships' => $starshipCount,
             'myShip' => $myShip,
         ]);
